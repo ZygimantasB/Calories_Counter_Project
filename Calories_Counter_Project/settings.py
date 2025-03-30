69ed7e1d1ccb32c13a8d7b7d3e6bfbaf597238c0
ZygimantasB <brinkis.zygimantas@gmail.com>
2025-03-30 23:13:54 +0300

    my home page is working now

--- Calories_Counter_Project/settings.py
+++ Calories_Counter_Project/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'Calories_Counter_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,13 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = BASE_DIR / "uploads"
-
 MEDIA_URL = "/files/"
 
 # Default primary key field type
