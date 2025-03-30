fcf731a139511498d73b3e87c9b4cd39b6ba67e9
ZygimantasB <brinkis.zygimantas@gmail.com>
2025-03-30 23:14:40 +0300

    my home page is working now

--- count_calories_app/views.py
+++ count_calories_app/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 import os
 
 def home(request):
-    # Debugging info
-    print("Attempting to render home.html")
-    template_path = os.path.join(os.path.dirname(__file__), 'templates', 'count_calories_app', 'home.html')
-    print(f"Template path: {template_path}")
-    print(f"Template exists: {os.path.exists(template_path)}")
-
     return render(request, 'count_calories_app/home.html')
